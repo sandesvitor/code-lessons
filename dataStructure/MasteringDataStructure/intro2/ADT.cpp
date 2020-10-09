da1319c012591d98162c06c2e529d5a4bdba06d7
Vitor Sandes <sandesvitor@gmail.com>
2020-10-09 00:51:29 -0300

    Updating

--- dataStructure/MasteringDataStructure/intro2/ADT.cpp
+++ dataStructure/MasteringDataStructure/intro2/ADT.cpp
@@ -1,9 +1,25 @@
 /*
     ADT - ABSTRACT DATATYPE
 
-    "Data Type":
-        1) Representation of Data;
-        2) Operations on Data
+    "Primitiv Data Type":
+        1) Representation of Data (int, float, double, boolean,...);
+        2) Operations on Data (+, -, *, ++, --, ...)
+
+    
+    "Abstract Data Type":
+
+    Ex: List ---> 8, 3, 9, 4, 6, 10, 12
+                  0  1  2  3  4   5   6
+        
+        Data: 1 - Space for storing element;
+              2 - Capacity;
+              3 - Size;
+
+        Operations: add(x)
+                    remove()
+                    search(key)
+                    ...
+        How to Store this elements? ---> Arrays or Linked Lists!
 */
 
 #include <stdio.h>
